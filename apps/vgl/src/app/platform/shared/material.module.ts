69f7905e2caf021db446f487bdd97de97c7c040b
Fred John <fredericoduarte87@gmail.com>
2022-07-24 20:18:56 +0100

    Add some more components themes, typography and expand on styling game-detail component

--- apps/vgl/src/app/platform/shared/material.module.ts
+++ apps/vgl/src/app/platform/shared/material.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
   imports: [
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
   ]
 })
 export class MaterialModule {}
